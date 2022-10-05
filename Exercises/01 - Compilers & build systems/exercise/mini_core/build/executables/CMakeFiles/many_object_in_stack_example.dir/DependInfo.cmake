5d33bea044d25876a6fcd817af357ea6deaa3aec
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2022-10-06 00:43:08 +0200

    RP: Add first homework (50%)
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

---
+++ Exercises/01 - Compilers & build systems/exercise/mini_core/build/executables/CMakeFiles/many_object_in_stack_example.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/slimshadys/Università/Personal/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/executables/many_object_in_stack_example.cpp" "/home/slimshadys/Università/Personal/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/build/executables/CMakeFiles/many_object_in_stack_example.dir/many_object_in_stack_example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
