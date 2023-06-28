497e4691bc3affbff98dc7452458ed4b655f8939
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2023-06-28 16:31:11 +0200

    RP: Few updates to Exercise 01 & add CPP Inheritance
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

---
+++ Exercises/01 - Compilers & build systems/exercise/mini_core/out/executables/CMakeFiles/many_object_in_stack_example.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/executables/many_object_in_stack_example.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/executables/CMakeFiles/many_object_in_stack_example.dir/many_object_in_stack_example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../src"
+  "../src/data_structures"
+  "../src/geometry"
+  "../src/messages"
+  "../src/utils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/messages/base_sensor_message.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/messages/base_sensor_message.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/messages/odometry_message.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/messages/odometry_message.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/messages/pose_message.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/messages/pose_message.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/blob.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/blob.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/deserializer.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/deserializer.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/id_context.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/id_context.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/id_placeholder.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/id_placeholder.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/identifiable.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/identifiable.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/object_data.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/object_data.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/property.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/property.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/property_container.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/property_container.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/property_container_manager.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/property_container_manager.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/property_eigen.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/property_eigen.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/property_identifiable.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/property_identifiable.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/serializable.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/serializable.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/serialization_context.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/serialization_context.cpp.o"
+  "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/src/utils/serializer.cpp" "/home/lattinone/RobotProgramming/Exercises/01 - Compilers & build systems/exercise/mini_core/out/src/CMakeFiles/core_library.dir/utils/serializer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "core_library_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../src"
+  "../src/data_structures"
+  "../src/geometry"
+  "../src/messages"
+  "../src/utils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
