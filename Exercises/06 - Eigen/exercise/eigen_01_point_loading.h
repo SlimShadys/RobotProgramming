e3fab2310d0cbf630fb23703b1b29469e0ec6e7a
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2023-07-01 16:17:23 +0200

    RP: Add solution for Point2D/3D loading
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- Exercises/06 - Eigen/exercise/eigen_01_point_loading.h
+++ Exercises/06 - Eigen/exercise/eigen_01_point_loading.h
@@ -5,6 +5,7 @@
 #include "eigen_00_basics.h"
 #include <iostream>
 #include <fstream>
+#include <random>
 
 // we define a std vector of 3d points, the allocator helps
 // the optimizer for aligned accesses. Required with -O3 (otherwise the program crashes)
@@ -20,35 +21,75 @@ using Vector2fVector=std::vector<Vector2f, Eigen::aligned_allocator<Vector2f> >;
 // how many rows the matrix has
 
 template <typename ContainerType_>
-int loadPoints(ContainerType_& dest, std::istream& is) {
-  using PointType = typename ContainerType_::value_type;
-  
-  constexpr int dim=PointType::RowsAtCompileTime;
-  while (is.good()) {
-    PointType v;
-    // we read one element at t time, and we put it in v(i)
-    for (int i=0; i<dim; ++i) {
-      is >> v(i);
+int loadPoints(ContainerType_& dest, std::istream& is, int dimension) {
+    using PointType = typename ContainerType_::value_type;
+
+    if (dimension <= 0 || dimension > PointType::RowsAtCompileTime) {
+        std::cerr << "Invalid dimension specified!" << std::endl;
+        return 0;
     }
-    if(! is.good())
-      break;
-    // at the end we push back the element in the container
-    dest.push_back(v);
-  }
-  return dest.size();
+
+    while (is.good()) {
+        PointType v;
+
+        for (int i = 0; i < dimension; ++i) {
+            is >> v(i);
+        }
+
+        if (!is.good())
+            break;
+
+        dest.push_back(v);
+    }
+
+    return dest.size();
 }
 
 template <typename ContainerType_>
 int savePoints(std::ostream& os, const ContainerType_& src) {
   using PointType= typename ContainerType_::value_type;
   int dim=PointType::RowsAtCompileTime;
-
   // we use the ":" to iterate in a container
   for (const auto& v: src) {
     for (int i=0; i<dim; ++i) {
-      os << v(i) << " ";
+      std::cout << "v(" <<i<< "): " << v(i) << " ";
     }
-    os << std::endl;
+    std::cout << std::endl;
   }
   return src.size();
+}
+
+float getRandomFloat(float min, float max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min, max);
+    return dis(gen);
+}
+
+// Function to generate a random Vector2f with values between min and max for each element
+Vector2f getRandomVector2f(float min, float max) {
+    Vector2f randomVec;
+    randomVec << getRandomFloat(min, max), getRandomFloat(min, max);
+    return randomVec;
+}
+
+// Function to generate a random Vector2f with values between min and max for each element
+Vector3f getRandomVector3f(float min, float max) {
+    Vector3f randomVec;
+    randomVec << getRandomFloat(min, max), getRandomFloat(min, max), getRandomFloat(min, max);
+    return randomVec;
+}
+
+bool compareVectors2D(const Vector2f& m, const Vector2f& n) {
+   // Generate p only once. Static as requested.
+   // We generate with numbers between -2 and 2.
+    static Vector2f p = getRandomVector2f(-2.0f, 2.0f);
+    return (p - m).transpose() * n < 0;
+}
+
+bool compareVectors3D(const Vector3f& m, const Vector3f& n) {
+   // Generate p only once. Static as requested.
+   // We generate with numbers between -2 and 2.
+    static Vector3f p = getRandomVector3f(-2.0f, 2.0f);
+    return (p - m).transpose() * n < 0;
 }
--- Exercises/06 - Eigen/exercise/eigen_01_point_loading_test.cpp
+++ Exercises/06 - Eigen/exercise/eigen_01_point_loading_test.cpp
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+using Matrix2d = Eigen::Matrix<float, 2,2>;
 using Vector2f = Eigen::Matrix<float, 2,1>;
 using Vector2fList = std::list<Vector2f, Eigen::aligned_allocator<Vector2f> >;
+using Vector3fList = std::list<Vector3f, Eigen::aligned_allocator<Vector3f> >;
 
 struct StuffWithEigen {
   char c;
@@ -16,48 +18,85 @@ struct StuffWithEigen {
 
 using StuffWithEigenVector = std::vector<StuffWithEigen>;
 
-
-
 int main(int argc, char** argv) {
-  cerr<< "the size is: " << sizeof(StuffWithEigen) << endl;
-  
+ 
+  // Just showing how push_back works with containers
   StuffWithEigenVector my_container;
   for (int i=0; i<100; ++i) {
     my_container.push_back(StuffWithEigen());
   }
 
-  for (const auto& item: my_container) {
-    cout << item.point1;
-    cout << item.point2;
-    cout << (item.point1+item.point2);
-    cout << endl;
+  if (argc<2) return -1;
+
+  int num_points2D = 0;
+  int num_points3D = 0;
+  Vector2fList points2D;
+  Vector3fList points3D;
+  std::ifstream is;
+
+  bool run = true;
+  while (1) {
+    cerr << "Sorting: 1 = 2D points; 2 = 3D points" << endl;
+    cerr << "Comparison formula: (p-m)^T * n < 0" << endl;
+    cerr << "-------------------------" << endl;
+    char s;
+    cin >> s;
+    switch (s) {
+      case '1':
+        cerr << "Sorting 2D points" << endl;
+        //is.open("test_data/points_3d.dat");
+        is.open(argv[1]);
+        num_points2D = loadPoints(points2D, is, 2);
+        cerr << "I read " << num_points2D << " points from the stream " << endl;
+        savePoints(std::cout, points2D);
+
+        // Sorting the points2D list based on the comparison formula
+        points2D.sort(compareVectors2D);
+
+        std::cerr << "-------------------------" << std::endl;
+        std::cerr << "------ After sort -------" << std::endl;
+
+        using PointType = typename Vector2fList::value_type;
+        for (const auto& v: points2D) {
+          for (int i=0; i<PointType::RowsAtCompileTime; ++i) {
+            std::cout << "v(" <<i<< "): " << v(i) << " ";
+          }
+          std::cout << std::endl;
+        }
+        std::cerr << "-------------------------" << std::endl;
+        is.close();
+        break;
+      case '2':
+        cerr << "Sorting 3D points" << endl;
+        //is.open("test_data/points_3d.dat");
+        is.open(argv[1]);
+        num_points3D = loadPoints(points3D, is, 3);
+        cerr << "I read " << num_points3D << " points from the stream " << endl;
+        savePoints(std::cerr, points3D);
+
+        // Sorting the points3D list based on the comparison formula
+        points3D.sort(compareVectors3D);
+
+        std::cerr << "-------------------------" << std::endl;
+        std::cerr << "------ After sort -------" << std::endl;
+
+        using PointType3 = typename Vector3fList::value_type;
+        for (const auto& v: points3D) {
+          for (int i=0; i<PointType3::RowsAtCompileTime; ++i) {
+            std::cout << "v(" <<i<< "): " << v(i) << " ";
+          }
+          std::cout << std::endl;
+        }
+        std::cerr << "-------------------------" << std::endl;
+        is.close();
+        break;
+      default:
+        cout << "Invalid option, exiting" << endl;
+        run = false;
+        break;
+    }
+    if (!run) break;
   }
-  
-  if (argc<2)
-    return -1;
-  // we open a file stream;
 
-  
-  std::ifstream is(argv[1]);
-  Vector2fList points;
-  int num_points = loadPoints(points, is);
-  cerr << "I read" << num_points << "from the stream " << endl;
-  savePoints(std::cout, points);
-
-  
-  // std::ifstream is(argv[1]);
-  // Vector3fVector points;
-  // int num_points = loadPoints(points, is);
-  // cerr << "I read" << num_points << "from the stream " << endl;
-  // savePoints(std::cerr, points);
-
-
-  /* TODO for you 
-
-     1. change the above program to read n points in 2D
-     2. change the program to read n points in 2D and 3D from a std::list
-
-   */
   return 0;
 }
-
