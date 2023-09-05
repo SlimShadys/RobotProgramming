d6b51c1d5f3b017b36b7c16384bb32c3492d3e26
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2023-09-05 12:21:16 +0200

    RP: Initialize a simple ROS project
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

---
+++ Exercises/10 - ROS & Rviz/Exercises/my_first_ros_project/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lattinone/Desktop/RobotProgrammingPersonal/Exercises/10 - ROS & Rviz/Exercises/my_first_ros_project/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/lattinone/RobotProgramming/Exercises/10 - ROS & Rviz/Exercises/my_first_ros_project/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lattinone/Desktop/RobotProgrammingPersonal/Exercises/10 - ROS & Rviz/Exercises/my_first_ros_project/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lattinone/Desktop/RobotProgrammingPersonal/Exercises/10 - ROS & Rviz/Exercises/my_first_ros_project/src:$ROS_PACKAGE_PATH"
